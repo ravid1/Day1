926059c503ff110646cd70f56a046a5aa4a1cd74
ravid1 <ravidpo@gmail.com>
2018-04-23 15:33:25 +0300

    version 2

--- stam.js
+++ stam.js
@@ -1,2 +1,14 @@
-console.log('hello!!!');
-console.log('hello!!!');
+const letters = ['a','b','c','d','e','f'];
+
+function cube(){
+    var number = Math.floor(Math.random()*5+1);
+    return letters[number];
+}
+
+console.log(cube());
+console.log(cube());
+console.log(cube());
+console.log(cube());
+console.log(cube());
+console.log(cube());
+console.log(cube());
